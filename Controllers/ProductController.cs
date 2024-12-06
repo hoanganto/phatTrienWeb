2fbcad5cd1aaa9c3c6c9ae6f5b143c744c48b2c5
tohoangan <an.th14640@sinhvien.hoasen.edu.vn>
2024-12-06 16:18:55 +0700

    aa
    aa

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StationShop.AppData;
+using StationShop.Models;
+
+namespace StationShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDBContext _db;
+
+        public ProductController( AppDBContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product? p = _db.Products.Find(id);
+            return View(p);
+        }
+    }
+}
